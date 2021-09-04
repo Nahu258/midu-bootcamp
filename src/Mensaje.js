6912f9017a1cf3634e79dddf1654ab3b6a5a4079
Nahu <nahugc258@gmail.com>
2021-09-04 19:01:27 -0300

    first commit

---
+++ src/Mensaje.js
@@ -0,0 +1,11 @@
+const Mensaje = (props) => {
+    return <h1 style={{color: props.color}}>
+        {props.mensaje}
+        </h1>
+}
+
+// const Suma = (a, b) => {
+//     return (a+b)
+// }
+
+export default Mensaje
